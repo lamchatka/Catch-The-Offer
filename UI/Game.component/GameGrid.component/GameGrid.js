2a3e27cdde09bed81febb586f054d72655d7a163
lamchatka <maxprolife@icloud.com>
2024-02-10 20:28:30 +0300

    GameGrid and Row refactoring

--- UI/Game.component/GameGrid.component/GameGrid.js
+++ UI/Game.component/GameGrid.component/GameGrid.js
@@ -1,17 +1,13 @@
 import {data} from "../../../data/data.js";
 import {Row} from "./Row.component/Row.js";
-import {Cell} from "./Cell.component/Cell.js";
+
 
 export function GameGrid() {
     const gridElement = document.createElement('table');
     gridElement.className = 'gridElement';
 
     for (let y = 0; y < data.gridRowsCount; y++) {
-        const rowElement = Row()
-        for (let x = 0; x < data.gridColumnsCount; x++) {
-            const cellElement = Cell(x, y);
-            rowElement.append(cellElement);
-        }
+        const rowElement = Row(y);
         gridElement.append(rowElement);
     }
 
--- UI/Game.component/GameGrid.component/Row.component/Row.js
+++ UI/Game.component/GameGrid.component/Row.component/Row.js
@@ -1,4 +1,11 @@
-export function Row() {
+import {data} from "../../../../data/data.js";
+import {Cell} from "../Cell.component/Cell.js";
+
+export function Row(y) {
     const row = document.createElement('tr');
+    for (let x = 0; x < data.gridColumnsCount; x++) {
+        const cellElement = Cell(x, y);
+        row.append(cellElement);
+    }
     return row;
 }
